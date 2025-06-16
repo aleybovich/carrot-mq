ce7a3a11dad55e6f38d40fb5c84076c48715f96d
leyboan1 <andrey.leybovich@novartis.com>
2025-06-16 21:34:18 +0700

    Added nil logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,5 +1,7 @@
 package logger
 
+import "fmt"
+
 // Logger interface definition
 type Logger interface {
 	Fatal(format string, a ...any)
@@ -8,3 +10,21 @@ type Logger interface {
 	Info(format string, a ...any)
 	Debug(format string, a ...any)
 }
+
+// NilLogger is a logger implementation that doesn't write any logs
+type NilLogger struct{}
+
+// Fatal does nothing
+func (n *NilLogger) Fatal(format string, a ...any) { panic(fmt.Sprintf(format, a...)) }
+
+// Err does nothing
+func (n *NilLogger) Err(format string, a ...any) {}
+
+// Warn does nothing
+func (n *NilLogger) Warn(format string, a ...any) {}
+
+// Info does nothing
+func (n *NilLogger) Info(format string, a ...any) {}
+
+// Debug does nothing
+func (n *NilLogger) Debug(format string, a ...any) {}
